c85f8e06cae3c28508edc5008d9b9a0d62ca9210
fengxiao <foxaiwushang@sina.com>
2020-12-22 20:18:44 +0800

    commit code

--- src/utils/static.js
+++ src/utils/static.js
@@ -2,8 +2,8 @@
  * @Author: FengXiao
  * @Date: 2020-12-20 11:22:30
  * @LastEditors: FengXiao
- * @LastEditTime: 2020-12-22 15:10:55
+ * @LastEditTime: 2020-12-22 20:18:26
  */
-export const BACKGROUND_VIDEO_URL = require('@/assets/video/bg_video.mp4');
+export const BACKGROUND_VIDEO_URL = '';
 
 export const BACKGROUND_VIDEO_POSTER_URL = '';
